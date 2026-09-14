f20894efa088263f156ea1f3f303787c4579355b
Rishi Pandey <rishipandeycs@gmail.com>
2026-09-14 10:48:27 +0530

    Time: 0 ms (100%), Space: 43.2 MB (51.56%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.Length-1;
+
+        int mid;
+
+        while(low <= high){
+
+            mid = (low+high)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid +1;
+            }
+            else if(nums[mid] > target){
+                high = mid -1;
+            }
+
+        }
+            return low;
+
+    }
+}
