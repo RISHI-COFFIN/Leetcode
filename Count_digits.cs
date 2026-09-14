633aa7a0a14c08498206d77c98ba57d9a2bfa0a7
Rishi Pandey <rishipandeycs@gmail.com>
2026-09-14 10:10:21 +0530

    Add files via upload

---
+++ Count_digits.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths
+{
+    internal class Count_digits
+    {
+        public int Count_digit(int x)
+        {
+
+            int count = 0;
+
+            while (x > 0)
+            {
+       
+
+                count++;
+
+                x = x / 10;
+
+              
+            }
+
+            return count;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+            Count_digits obj = new Count_digits();
+
+            Console.WriteLine(obj.Count_digit(667));
+        }
+
+    }
+}
