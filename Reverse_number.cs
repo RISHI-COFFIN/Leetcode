bf950d0ccfb9d80752567a99d51465498f61716e
Rishi Pandey <rishipandeycs@gmail.com>
2026-09-14 10:01:41 +0530

    Add files via upload

---
+++ Reverse_number.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths
+{
+    internal class Reverse_number
+    {
+
+        public int reverse(int x)
+        {
+
+            int rev = 0;
+
+            while(x > 0)
+            {
+                int last = x % 10;
+
+                x = x / 10;
+
+                rev = (rev * 10) + last;
+            }
+
+            return rev;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            
+            Reverse_number obj = new Reverse_number();
+
+            Console.WriteLine(obj.reverse(27845));
+        }
+
+
+    }
+}
